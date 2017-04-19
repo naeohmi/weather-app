fb97666a7920965967443f1818985bcb1238071f
naeohmi <naeohmi@gmail.com>
2017-04-19 19:47:55 -0400

    werkin on it

--- app/main.js
+++ app/main.js
@@ -10,3 +10,42 @@
 // 		e. Max temp
 // 5. Have the temperature turn blue if under 40, and red if above 90
 
+//api call with key:
+//api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3bfe1bb3354e449001b55ef397099f50
+console.log('aliveee');
+
+class Weather {
+    constructor(cityName, currentTemp, minTemp, maxTemp, wDesc) {
+        this.cityName = cityName;
+        this.currentTemp = currentTemp;
+        this.minTemp = minTemp;
+        this.maxTemp = maxTemp;
+        this.wDesc = wDesc;
+    }
+    submitZip() {
+        console.log('yay');
+        let zipCode = document.querySelector('#zip');
+        document.querySelector('#click').addEventListener('click', function() {
+            console.log('boom!');
+            
+
+        })
+        
+    }
+    apiCall() {
+        $(() => {
+            $.ajax({
+                url: 'api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3bfe1bb3354e449001b55ef397099f50',
+                context: document.body
+            }).done((d) => {
+                $(this).append(d.weather);
+            }).always(() => {
+                console.log('run after all');
+            })
+        })
+    }
+}
+
+let w = new Weather;
+w.submitZip();
+//w.apiCall();
