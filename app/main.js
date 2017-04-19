e7a78b090875c58c7017b0b99d546a7a7fc138ad
naeohmi <naeohmi@gmail.com>
2017-04-19 08:59:22 -0400

    comments

---
+++ app/main.js
@@ -0,0 +1,12 @@
+// HW Requirements:
+// 1. An input field for a user to enter a zip code
+// 2. A submit button, when clicked makes a GET request to fetch the weather data from the API
+// 3. Render data on the page
+// 4. Data to be rendered:
+// 		a. City name
+// 		b. Current temperature
+// 		c. Weather description
+// 		d. Min temp
+// 		e. Max temp
+// 5. Have the temperature turn blue if under 40, and red if above 90
+
