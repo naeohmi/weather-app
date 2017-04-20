0aafd897d74daed2727dcfc6842e518e841ab11c
naeohmi <naeohmi@gmail.com>
2017-04-19 20:27:06 -0400

    made em constsss

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 // HW Requirements:
 // 1. An input field for a user to enter a zip code
-// 2. A submit button, when clicked makes a GET request to fetch the weather data from the API
+// 2. A submit button, when clicked,
+// makes a GET request to fetch the weather data from the API
 // 3. Render data on the page
 // 4. Data to be rendered:
 // 		a. City name
@@ -9,33 +10,42 @@
 // 		d. Min temp
 // 		e. Max temp
 // 5. Have the temperature turn blue if under 40, and red if above 90
-
-//api call with key:
-//api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3bfe1bb3354e449001b55ef397099f50
 console.log('aliveee');
 
-class Weather {
-    constructor(cityName, currentTemp, minTemp, maxTemp, wDesc) {
-        this.cityName = cityName;
-        this.currentTemp = currentTemp;
-        this.minTemp = minTemp;
-        this.maxTemp = maxTemp;
-        this.wDesc = wDesc;
-    }
-    submitZip() {
+const urlFront = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const urlBack = 'APPID=3bfe1bb3354e449001b55ef397099f50';
+const button = document.getElementById('submit');
+
+$(function () {
+    const cityName = document.getElementById('cityName');
+    const curTemp = document.getElementById('curTemp')
+    const minTemp = document.getElementById('minTemp');
+    const maxTemp = document.getElementById('maxTemp');
+    const wDesc = document.getElementById('wDesc');
+
         console.log('yay');
         let zipCode = document.querySelector('#zip');
-        document.querySelector('#click').addEventListener('click', function() {
+        document.querySelector('#click').addEventListener('click', function () {
             console.log('boom!');
-            
+            submitZip();
+
 
         })
-        
+
+    
+        let submitZip = () => {
+
     }
-    apiCall() {
+    let url = () => {
+        const url1 = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+        const url2 = '524901&APPID=3bfe1bb3354e449001b55ef397099f50';
+        let inputZip = document.querySelector('#zip').value;
+        console.log(inputZip);
+    }
+    let apiCall = () => {
         $(() => {
             $.ajax({
-                url: 'api.openweathermap.org/data/2.5/forecast?id=524901&APPID=3bfe1bb3354e449001b55ef397099f50',
+                url: '',
                 context: document.body
             }).done((d) => {
                 $(this).append(d.weather);
@@ -44,8 +54,5 @@ class Weather {
             })
         })
     }
-}
 
-let w = new Weather;
-w.submitZip();
-//w.apiCall();
+});
