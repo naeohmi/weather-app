19abb54c603633ba6355417b0a3b53b86ee802e7
naeohmi <naeohmi@gmail.com>
2017-04-20 11:38:13 -0400

    degrees

--- app/main.js
+++ app/main.js
@@ -11,11 +11,10 @@
 // 		e. Max temp
 // 5. Have the temperature turn blue if under 40, and red if above 90
 
-const u0 = 'https://accesscontrolalloworiginall.herokuapp.com/';
 //save url to GET weather using zip code from the API
 const u1 = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 //save personal key to attach to end of URL for API use
-const u2 = '&APPID=3bfe1bb3354e449001b55ef397099f50';
+const u2 = '&APPID=3bfe1bb3354e449001b55ef397099f50&units=imperial';
 
 //create click event when zip code is input and submit button is pressed
 //use the input/submitted zip code to find the weather data  
@@ -29,7 +28,7 @@ let click = () => {
 //using the zipcode, add the necessary data step by step, to the innerHTML of the page
 //although fetch is still 'experimental' seemed like a fun/cool chance to use it here
 let weather = (z) => {
-    fetch(u0 + u1 + z + u2) //the full URL with zipcode
+    fetch(u1 + z + u2) //the full URL with zipcode
     .then( (r) => {
         return r.json(); //grab the json data
     }).then( (d) => {
@@ -40,16 +39,16 @@ let weather = (z) => {
         wDesc.innerHTML = 'Weather Description: ' + d.weather[0].description;
 
         let cTemp = document.createElement('h3');
-        let c = Math.floor(d.main.temp * 9/5 - 459.67); //kelvin to F
-        cTemp.innerHTML = `Current Temp: ${c}F`;
+        let c = d.main.temp; 
+        cTemp.innerHTML = `Current Temp: ${c}&deg;F`;
 
         let minTemp = document.createElement('h3');
-        let min = Math.floor(d.main.temp_min * 9/5 - 459.67); //kelvin to F
-        minTemp.innerHTML = `Minimum Temperature: ${min}F`;
+        let min = d.main.temp_min; 
+        minTemp.innerHTML = `Minimum Temperature: ${min}&deg;F`;
         
         let maxTemp = document.createElement('h3');
-        let max = Math.floor(d.main.temp_max * 9/5 - 459.67); //kelvin to F
-        maxTemp.innerHTML = `Maximum Temperature: ${max}F`;
+        let max = d.main.temp_max;
+        maxTemp.innerHTML = `Maximum Temperature: ${max}&deg;F`;
         
         //if weather is hot (< 40F) change color to red
         //if weather is cold (> 90F) change color to blue
