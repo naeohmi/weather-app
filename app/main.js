28067a03fbf1f45b3475c5c7f668ed5811a4f4ad
naeohmi <naeohmi@gmail.com>
2017-04-19 20:43:17 -0400

    rethink and pivot js

--- app/main.js
+++ app/main.js
@@ -11,48 +11,28 @@
 // 		e. Max temp
 // 5. Have the temperature turn blue if under 40, and red if above 90
 console.log('aliveee');
+const u1 = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const u2 = '&APPID=3bfe1bb3354e449001b55ef397099f50';
 
-const urlFront = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-const urlBack = 'APPID=3bfe1bb3354e449001b55ef397099f50';
-const button = document.getElementById('submit');
+let click = () => {
+    const submit = document.getElementById('submit');
+    submit.addEventListener('click', () => {
+        let z = document.getElementById('zip').value;
+        console.log(z);
+        weather(z);
+    });
+};
+click();
 
-$(function () {
-    const cityName = document.getElementById('cityName');
-    const curTemp = document.getElementById('curTemp')
-    const minTemp = document.getElementById('minTemp');
-    const maxTemp = document.getElementById('maxTemp');
-    const wDesc = document.getElementById('wDesc');
-
-        console.log('yay');
-        let zipCode = document.querySelector('#zip');
-        document.querySelector('#click').addEventListener('click', function () {
-            console.log('boom!');
-            submitZip();
-
-
-        })
-
-    
-        let submitZip = () => {
-
-    }
-    let url = () => {
-        const url1 = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-        const url2 = '524901&APPID=3bfe1bb3354e449001b55ef397099f50';
-        let inputZip = document.querySelector('#zip').value;
-        console.log(inputZip);
-    }
-    let apiCall = () => {
-        $(() => {
-            $.ajax({
-                url: '',
-                context: document.body
-            }).done((d) => {
-                $(this).append(d.weather);
-            }).always(() => {
-                console.log('run after all');
-            })
-        })
-    }
-
-});
+let weather = (z) => {
+    fetch(u1 + z + u2)
+    .then(function (r) {
+        return r.json();
+    }).then(function (d) {
+        let city = document.createElement('h3');
+        city.innerHTML = 'City: ' + d.name;
+        city.setAttribute('id', 'city');
+        document.getElementById('container').appendChild(city);
+        console.log(city);
+    })
+};
